d5fd535b3722715b9800f54ec7477b37da265119
unknown <faraazm>
2017-12-30 20:17:38 -0500

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+const port = process.env.port || 3000;
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+app.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+})
+
+module.exports = app;
